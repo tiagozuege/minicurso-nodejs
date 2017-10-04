a11028dc62a842199a59e74331f97e5a663d4ad6
TIAGO JASPER ZUEGE <tiago.zuege@gmail.com>
2017-10-04 20:24:34 -0300

    Commit inicial

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const express = require('express');		//importa o framework 'express'
+
+
+let app = express();			//inicia o framework 'express'
+
+app.use(express.static('public'));						//passa como parametro os arquivos estaticos utilizados
+
+app.get('/teste', (req, res) => {
+
+	res.send('it works!');		//envia como resposta no browser a msg
+});
+
+app.listen(3000, () => {
+	console.log('UP');
+});
